56a23b1658c26d8c53fa95598590da4c7dcfc0d5
Shayne Kasai <shaynekasai@gmail.com>
2013-11-15 07:50:05 -0800

    Update URL.php
    Cleanup

--- lib/UrlShortener/URL.php
+++ lib/UrlShortener/URL.php
@@ -12,10 +12,10 @@ namespace lib\UrlShortener;
 
 class URL {
     /**
-     *  I want to support more but crc32 for now, returns an 8 character rep of the url
+     * I want to support more but crc32 for now, returns an 8 character rep of the url
      *
-     *  @return string
-     *  @param $url  
+     * @return string
+     * @param $url  
      */
     public function shortenCRC($url) {
        return( hash('crc32', $url) ); 
