64f07491cbce01c9c7fbffe696bb9bdad4a3aa31
Shayne Kasai <shaynekasai@gmail.com>
2013-11-19 08:41:36 -0800

    Update Dispatch.php
    Fixing URL check

--- lib/UrlShortener/Dispatch.php
+++ lib/UrlShortener/Dispatch.php
@@ -7,27 +7,27 @@
 use lib\UrlShortener\URLService as URLService;
 
 class Dispatch {
-	private  $Service;
+    private  $Service;
 	
-	function __construct() {
-		$this->Service =  new URLService();
-	}
+    function __construct() {
+        $this->Service =  new URLService();
+    }
 	
-	/* 
-	 * This is meant to handle any get requests for the service
-	 *
-	 * @param array
-	 * @returns 
-	 */
+    /* 
+     * This is meant to handle any get requests for the service
+     *
+     * @param array
+     * @returns 
+     */
     function get($params='') {
     	$hash = (isset($_GET['id']) == true) ? (string)$_GET['id'] : $params['hash'];
     	$objURL = $this->Service->getURL($hash);
     	
     	if(isset($_GET['id'])) {
-			echo $objURL == false ? json_encode(array("status" => "error")) : json_encode(array("url" => $objURL->url));
-		} else {
-			return $objURL == false ? json_encode(array("status" => "error"))  : json_encode(array("url" => $objURL->url));
-		}
+	    echo $objURL == false ? json_encode(array("status" => "error")) : json_encode(array("url" => $objURL->url));
+        } else {
+	    return $objURL == false ? json_encode(array("status" => "error"))  : json_encode(array("url" => $objURL->url));
+        }
     }
     
     /* 
@@ -41,17 +41,14 @@ class Dispatch {
     	$url = (string)$_POST['url'];
     	if (preg_match("/^https?:\/\/.{1,70}\.[a-zA-Z.]{2,5}.{0,1000}$/", $url)) {
     	
-    		$hash = $this->Service->shorten($url);
-    		$friendly_url = 'http://' . $_SERVER["HTTP_HOST"] . '/' . $hash;
-			echo json_encode(array("status" => "success", "friendly_url" => $friendly_url, "hash" => $hash, "url" => $url));
+    	    $hash = $this->Service->shorten($url);
+    	    $friendly_url = 'http://' . $_SERVER["HTTP_HOST"] . '/' . $hash;
+            echo json_encode(array("status" => "success", "friendly_url" => $friendly_url, "hash" => $hash, "url" => $url));
 		
     	} else {
-    		echo json_encode(array("status" => "error", "message" => "Invalid URL, please enter a valid URL, example: http://www.foo.com"));
+    	    echo json_encode(array("status" => "error", "message" => "Invalid URL, please enter a valid URL, example: http://www.foo.com"));
     		
-    		
-		
-    		
-		}
+        }
     }
     function put() {}
     function delete() {}
