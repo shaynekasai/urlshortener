0849b749eda374b734f125fdf8d266edca36185b
Shayne Kasai <shaynekasai@gmail.com>
2013-11-19 08:40:13 -0800

    Update Dispatch.php
    Changed to regex checking for URL

--- lib/UrlShortener/Dispatch.php
+++ lib/UrlShortener/Dispatch.php
@@ -7,18 +7,18 @@
 use lib\UrlShortener\URLService as URLService;
 
 class Dispatch {
-    private  $Service;
+	private  $Service;
 	
-    function __construct() {
+	function __construct() {
 		$this->Service =  new URLService();
-    }
+	}
 	
-    /* 
-     * This is meant to handle any get requests for the service
-     *
-     * @param array
-     * @returns 
-     */
+	/* 
+	 * This is meant to handle any get requests for the service
+	 *
+	 * @param array
+	 * @returns 
+	 */
     function get($params='') {
     	$hash = (isset($_GET['id']) == true) ? (string)$_GET['id'] : $params['hash'];
     	$objURL = $this->Service->getURL($hash);
@@ -31,20 +31,26 @@ class Dispatch {
     }
     
     /* 
-     * This is meant to handle any post requests for the service, but mainly to ship and shorten.
-     *
-     * @param 
-     * @returns 
-     */
+	 * This is meant to handle any post requests for the service, but mainly to ship and shorten.
+	 *
+	 * @param 
+	 * @returns 
+	 */
     function post() {
     	
     	$url = (string)$_POST['url'];
-    	if (filter_var($url, FILTER_VALIDATE_URL)) {
+    	if (preg_match("/^https?:\/\/.{1,70}\.[a-zA-Z.]{2,5}.{0,1000}$/", $url)) {
+    	
     		$hash = $this->Service->shorten($url);
     		$friendly_url = 'http://' . $_SERVER["HTTP_HOST"] . '/' . $hash;
 			echo json_encode(array("status" => "success", "friendly_url" => $friendly_url, "hash" => $hash, "url" => $url));
+		
     	} else {
     		echo json_encode(array("status" => "error", "message" => "Invalid URL, please enter a valid URL, example: http://www.foo.com"));
+    		
+    		
+		
+    		
 		}
     }
     function put() {}
